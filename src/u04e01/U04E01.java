c8fbbc64283ef2734ce913d72b21578ec5396485
maxher73 <maxher73@yahoo.com.ar>
2020-03-23 16:26:16 -0300

    Probando de subir los ejercicios de Codos a Codo a Git

---
+++ src/u04e01/U04E01.java
@@ -0,0 +1,16 @@
+
+package u04e01;
+
+import java.util.Scanner;
+
+public class U04E01 {
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese area del cuadrado");
+        Scanner entrada= new Scanner(System.in);
+        double area = entrada.nextDouble();
+        double lado = Math.sqrt(area);
+        double perimetro = lado*4;
+        System.out.println("El perimetro del cuadrado es: "+perimetro);
+    }
+}
